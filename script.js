b30256a217908448469c807e349e1db7c82d6a16
amrithamanika <145197817+amrithamanika@users.noreply.github.com>
2024-01-20 15:28:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+// "use strict"
+// a=5;
+// console.log(a);
+
+// var Fname="Tiya"
+// console.log(Fname.length);
+// console.log(typeof(Fname));
+
+// var data =" This is a txt message "
+// console.log(data.length);
+// console.log(data.indexOf('txt'));
+// console.log(data.replace("txt","text"));
+// console.log(data);
+// console.log(data.split(','));
+// console.log(data.trim());
+// console.log(data);
+// console.log(data.trimStart());
+
+
+
+
+
+// let fruits =["Apple","Mango","Orange","Grapes"];
+// console.log(Array.isArray(fruits));
+// // foreach
+// fruits.forEach(function(item){
+//     console.log(item);
+// })
+
+
+
+// let numb=[20,30,58,65];
+// numb.forEach(function(item,ind){
+//     numb[ind]=item<50
+// })
+// console.log(numb);
+
+
+// var obj1=JSON.parse('{"name":"Tiya","age":10}');
+// console.log(obj1);
+// var obj2 =JSON.stringify(obj1);
+// console.log(obj2);
+
+
+// function sum(a,b){
+//     var s=a+b;
+//     console.log(`The sum is ${s}`);
+// }
+
+// sum(10,20);
+// var add =(c,d)=>{
+//     console.log(`The sum is ${c+d}`); 
+// }
+// add(10,20);
+// var sub =(m,n)=>{
+//     console.log(`The difference is ${m-n}`); 
+// }
+// sub(15,5);
+
+
+// spread operator
+// var a=[5,6,7];
+// var b=[45,89];
+// var c=[...a,...b];
+// console.log(c);
+// var d=[2,3,...b];
+// console.log(d);
+
+
+
+// ARRAY CONSTRUCTING
+ var[q,r,s]=[2,3,4];
+ console.log(r);
+ var[p,,w]=[2,5,8];
+ console.log(w);
+ var[h,,j,...l]=[1,2,3,4,5,6,7];
+ console.log(l);
+ 
